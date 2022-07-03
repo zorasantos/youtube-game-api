e344ad918037216c93eba8d4e5ad1d1581afb9d8
zorasantos <zoranildo22@gmail.com>
2022-07-03 18:40:40 -0300

    config: complete config eslint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-export default app
+  console.log(`Example app listening on port ${port}`);
+});
